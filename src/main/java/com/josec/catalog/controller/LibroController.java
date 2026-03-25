835c8839da92b08db9a4f20be6070cd947a9dacf
joselcm26 <josehl269@gmail.com>
2026-03-25 17:43:48 +0100

    Endpoints CRUD creados

--- src/main/java/com/josec/catalog/controller/LibroController.java
+++ src/main/java/com/josec/catalog/controller/LibroController.java
@@ -1,11 +1,9 @@
 package com.josec.catalog.controller;
 
 import com.josec.catalog.model.Libro;
-import com.josec.catalog.controller.LibroController;
-
 import com.josec.catalog.repository.LibroRepository;
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,10 +19,39 @@ public class LibroController {
     public List<Libro> findAll() {
         return libroRepository.findAll(); //Busca en la BD y lo devuelve
     }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id) {
+        if(libroRepository.findById(id).isPresent()) {
+            Libro libro = libroRepository.findById(id).get();
+            return ResponseEntity.ok(libro);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable int id) {
+        if (libroRepository.findById(id).isPresent()) {
+            Libro libro = libroRepository.findById(id).get();
+            libroRepository.delete(libro);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
     @PostMapping // POST HTTP
     public Libro save(@RequestBody Libro libro) {
         return libroRepository.save(libro); //Guarda el libro que nos llega en la BD
     }
 
+    @PutMapping
+    public ResponseEntity<?> putBook(@RequestBody Libro libro) {
+        if (libroRepository.findById(libro.getId().intValue()).isPresent()){
+            Libro lib = libroRepository.findById(libro.getId().intValue()).get();
+            if(lib.getId() != 0){
+                libroRepository.save(libro);
+                return ResponseEntity.noContent().build();
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
